4adc4d62d067decdcb5ac5034f48e5b0f612c389
Qiao, Liyong <liyong.qiao@intel.com>
2017-03-07 16:54:53 +0800

    Revert "API: upgrade to use openapi"
    This reverts commit fe111737ac9af61bb65eec77056693b043eceab7.
    
    To make sure our code is fast-forward, revert this commit, the reason
    is go-restful-openapi is not ready yet to support swagger 2.0.
    
    But we should keep eye to switch swagger 2.0 version.

--- rdtagent.go
+++ rdtagent.go
@@ -6,8 +6,7 @@ import (
 	"net/http"
 
 	"github.com/emicklei/go-restful"
-	restfulspec "github.com/emicklei/go-restful-openapi"
-	"github.com/go-openapi/spec"
+	"github.com/emicklei/go-restful-swagger12"
 )
 
 func main() {
@@ -23,38 +22,18 @@ func main() {
 	// You need to download the Swagger HTML5 assets and change the FilePath location in the config below.
 	// Open http://localhost:8081/apidocs and enter http://localhost:8081/apidocs.json in the api input field.
 	// FIXME we should use config file for WebServicesUrl
-	config := restfulspec.Config{
+	config := swagger.Config{
 		WebServices:    wsContainer.RegisteredWebServices(), // you control what services are visible
-		WebServicesURL: "http://localhost:8081",
-		APIPath:        "/apidocs.json",
-		PostBuildSwaggerObjectHandler: enrichSwaggerObject}
+		WebServicesUrl: "http://localhost:8081",
+		ApiPath:        "/apidocs.json",
 
-	restfulspec.RegisterOpenAPIService(config, wsContainer)
+		// Optionally, specifiy where the UI is located
+		SwaggerPath: "/apidocs/",
+		// FIXME (eliqiao): this depends on https://github.com/swagger-api/swagger-ui.git need to copy dist from it
+		SwaggerFilePath: "/usr/local/share/go/src/github.com/wordnik/swagger-ui/dist"}
+	swagger.RegisterSwaggerService(config, wsContainer)
 
 	log.Printf("start listening on localhost:8081")
 	server := &http.Server{Addr: ":8081", Handler: wsContainer}
 	log.Fatal(server.ListenAndServe())
-}
-
-
-func enrichSwaggerObject(swo *spec.Swagger) {
-	swo.Info = &spec.Info{
-		InfoProps: spec.InfoProps{
-			Title:       "Rdtagent",
-			Description: "Resource for managing RDT",
-			Contact: &spec.ContactInfo{
-				Name:  "todo",
-				Email: "todo@intel.com",
-				URL:   "http://todo.org",
-			},
-			License: &spec.License{
-				Name: "MIT",
-				URL:  "http://mit.org",
-			},
-			Version: "1.0.0",
-		},
-	}
-	swo.Tags = []spec.Tag{spec.Tag{TagProps: spec.TagProps{
-		Name:        "cpuinfo",
-		Description: "Expose cpuinfo"}}}
 }
