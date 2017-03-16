bbfe53dbb31fcc005c1eca5a707b7bda30f7a8f0
Qiao, Liyong <liyong.qiao@intel.com>
2017-03-16 11:36:39 +0800

    API: CpuInfo
    Append cpuinfo endpoint, and swagger yaml.
    
    Fake some output before cgo work done.
    
    TODO: CpuinfoCapacityGet need to be done
    
    Change-Id: I2ad67db47dff9a81127f61fa6ac3754eb6d7bf60

--- api/v1/cpuinfo.go
+++ api/v1/cpuinfo.go
@@ -6,72 +6,151 @@ import (
 	"github.com/emicklei/go-restful/log"
 )
 
-// GET http://localhost:8081/cpuinfo
+// CPU Info
 
-type Cpuinfo struct {
-	Id   string `json:"socket_id,omitempty"`
-	Cpus string `json:"cpus,omitempty"`
+type Cacheinfo struct {
+	CacheLevel	string `json:"cache_level"`
+	CacheSize	uint32 `json:"cache_size"`
+	CacheWay	uint32 `json:"cache_way"`
 }
+
+type CpuInfo struct {
+	CpuNum	uint32 `json:"cpu_num"`
+	L2Cache 	Cacheinfo `json:"l2_cache"`
+	L3Cache 	Cacheinfo `json:"l3_cache"`
+}
+
+// CPU topology
+type Cpu struct {
+	Id	uint32 `json:"cpu_id"`
+}
+
+type Core struct {
+	Id 	uint32 `json:"core_id"`
+	Cpus	[]Cpu `json:"cpus"`
+
+}
+
+type Socket struct {
+	Id 	uint16 `json:"socket_id"`
+	Cores 	[]Core `json:"cores"`
+}
+
+type CpuTopo []Socket
+
 
 type CpuinfoResource struct {
 	// normally one would use DAO (data access object)
-	info map[string]Cpuinfo
+	info map[string]CpuTopo
+}
+
+// Fake cpu info
+func MakeCpuInfo() CpuInfo {
+	var c CpuInfo
+	c.CpuNum = 4
+	c.L2Cache = Cacheinfo{"l2", 1024, 20}
+	c.L3Cache = Cacheinfo{"l3", 56320, 20}
+	return c
+}
+
+// Fake cpu topology
+func MakeCpuTopo() CpuTopo {
+	// 1 sockets, 2 cores, 2 cpu per core
+	var t CpuTopo
+
+	var s Socket
+	s.Cores = make([]Core, 2)
+
+	cpu0 := Cpu {Id: 0}
+	cpu1 := Cpu {Id: 1}
+	cpu2 := Cpu {Id: 2}
+	cpu3 := Cpu {Id: 3}
+
+	s.Cores[0].Cpus = make([]Cpu, 2)
+	s.Cores[1].Cpus = make([]Cpu, 2)
+
+	s.Cores[0].Cpus[0] = cpu0
+	s.Cores[0].Cpus[1] = cpu1
+	s.Cores[1].Cpus[0] = cpu2
+	s.Cores[1].Cpus[1] = cpu3
+
+
+	t = append(t, s)
+	return t
 }
 
 func (cpuinfo CpuinfoResource) Register(container *restful.Container) {
 	ws := new(restful.WebService)
 	ws.
 		Path("/v1/cpuinfo").
-		Doc("Show the cup information of a host.").
+		Doc("Show the CPU information of a host").
 		Consumes(restful.MIME_JSON).
 		Produces(restful.MIME_JSON)
 
-	ws.Route(ws.GET("/").To(cpuinfo.getCpuinfo).
-		Doc("get cpuinfo").
-		Operation("getCpuinfo").
-		Writes(Cpuinfo{}))
+	ws.Route(ws.GET("/").To(cpuinfo.CpuinfoGet).
+		Doc("Get the CPU information, include level 2, level 3 cache informatione").
+		Operation("CpuinfoGet").
+		Writes(CpuInfo{}))
+	ws.Route(ws.GET("/topology").To(cpuinfo.CpuinfoTopologyGet).
+		Doc("Get the CPU topology information of sockets, core and thread in each core on the host machine").
+		Operation("CpuinfoTopologyGet").
+		Writes(CpuTopo{}))
 
-	ws.Route(ws.GET("/{socket-id}").To(cpuinfo.getSocketId).
-		Doc("get cpuinfo per socket id").
-		Param(ws.PathParameter("socket-id", "indentifier for a CPU socket").DataType("string")).
-		Operation("getSocketId").
-		Writes(Cpuinfo{}))
+	ws.Route(ws.GET("/capacity").To(cpuinfo.CpuinfoCapacityGet).
+		Doc("Get the capacities of the cache, memory").
+		Operation("CpuinfoCapacityGet").
+		Writes(Cpu{}))
 
 	container.Add(ws)
 }
 
-// GET http://localhost:8081/cpuinfo/
-func (cpuinfo CpuinfoResource) getCpuinfo(request *restful.Request, response *restful.Response) {
-
-	log.Printf("Received Request: %s", request.PathParameter("socket_id"))
-
-	res := make(map[string]Cpuinfo)
-
-	info := new(Cpuinfo)
-	info.Id = "1"
-
-	res["socket"] = *info
-
-	response.WriteEntity(res)
+// GET http://localhost:8081/cpuinfo
+func (cpuinfo CpuinfoResource) CpuinfoGet(request *restful.Request, response *restful.Response) {
+	c := MakeCpuInfo()
+	response.WriteEntity(c)
 }
 
-// GET http://localhost:8081/cpuinfo/{socket_id}
+// GET http://localhost:8081/cpuinfo/topology
+func (cpuinfo CpuinfoResource) CpuinfoTopologyGet(request *restful.Request, response *restful.Response) {
+	t := MakeCpuTopo()
+	response.WriteEntity(t)
+}
 
-func (cpuinfo CpuinfoResource) getSocketId(request *restful.Request, response *restful.Response) {
+// GET http://localhost:8081/cpuinfo/capacity
+
+func (cpuinfo CpuinfoResource) CpuinfoCapacityGet(request *restful.Request, response *restful.Response) {
 
 	log.Printf("In get socket id, received Request: %s", request.PathParameter("socket-id"))
 
-	info := new(Cpuinfo)
-	info.Id = "1"
+	// TODO
+
+	info := new(Cpu)
+	info.Id = 0
 
 	response.WriteEntity(info)
 }
 
-
-func (cpuinfo Cpuinfo) SwaggerDoc() map[string]string {
+func (c CpuInfo) SwaggerDoc() map[string]string {
     return map[string]string{
-        "":		"Cpuinfo doc",
-        "socket_id":	"ID of physical CPU socket",
-        "cpus":		"Cpu list which sits on this socket",
+	    "":	"Cpu Info",
+	    "CpuNum":	"cpu number",
+	    "L2Cache":	"level 2 cache information",
+	    "L3Cache":	"level 3 cache information",
+
+    }
+}
+
+func (c CpuTopo) SwaggerDoc() map[string]string {
+    return map[string]string{
+	    "":	"Cpu topology",
+
+    }
+}
+
+func (s Socket) SwaggerDoc() map[string]string {
+    return map[string]string{
+	    "":	"Cpu Scoket",
+	    "id":	"Socket id",
+
     }
 }
