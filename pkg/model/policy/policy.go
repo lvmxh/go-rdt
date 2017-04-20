77cb419bac042547a62ad837abb95cd5a69be25b
Qiao, Liyong <liyong.qiao@intel.com>
2017-04-20 20:09:27 +0800

    Model: import Policy
    Very simple implement for cache allocation policy.
    
    This path imports a Policy model.
    
    The Policy model support operate Policy from a policy.yaml:
        GetPolicy()
        UpdatePolicy()
    
    About the policy:
    
    etc/policy.yaml is a yaml file which contains for policy defined for
    each Intel platform, the file can be modified manually or by RESTAPI
    layer.
    
    The policy will do the enforcement while a new workload request send
    to RDTAgent.
    
    The request should contains {workload_type, policy, pids[cores]}
    
      workload_type: dpdk/other
      policy: gold/silver/cooper
      pids[cores]: pid or core list
    
    P.S, this patch request a new package yaml.v2 to parse yaml, added it
    in vendor.
    
    Along, added policy model unit test case.

---
+++ pkg/model/policy/policy.go
@@ -0,0 +1,100 @@
+package policy
+
+import (
+	"errors"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"sync"
+)
+
+var yaml_path = "./policy.yaml"
+
+type PolicyRule struct {
+	Dpdk  string
+	Other string
+}
+type PolicyType struct {
+	Size       uint
+	Percentage uint
+	Rule       PolicyRule
+	Pin_core   bool
+}
+
+type Policy struct {
+	Gold   PolicyType
+	Silver PolicyType
+	Copper PolicyType
+}
+
+type CATConfig struct {
+	Catpolicy map[string]*Policy `yaml:"catpolicy"`
+}
+
+var config *CATConfig
+var lock sync.Mutex
+
+// For testing
+func SetPolicyFilePath(path string) {
+	yaml_path = path
+}
+
+func LoadPolicy() (*CATConfig, error) {
+	r, err := ioutil.ReadFile(yaml_path)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+		return nil, err
+	}
+
+	c := CATConfig{}
+	err = yaml.Unmarshal(r, &c)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	return &c, err
+}
+
+// return a copy of policy, not a pointer
+func GetPolicy(cpu string) (Policy, error) {
+	lock.Lock()
+	defer lock.Unlock()
+	var err error
+
+	if config == nil {
+		config, err = LoadPolicy()
+		if err != nil {
+			return Policy{}, err
+		}
+	}
+
+	p, ok := config.Catpolicy[cpu]
+	if ok != true {
+		return Policy{}, errors.New("cpu doen't supported")
+	}
+
+	return *p, nil
+}
+
+// Write back policy to yaml file
+func UpdatePolicy(cpu string, p *Policy) error {
+	lock.Lock()
+	defer lock.Unlock()
+	if config == nil {
+		return errors.New("empty config file")
+	}
+
+	_, ok := config.Catpolicy[cpu]
+	if ok != true {
+		return errors.New("cpu doen't supported")
+	}
+
+	config.Catpolicy[cpu] = p
+
+	d, err := yaml.Marshal(config)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+		return err
+	}
+	return ioutil.WriteFile(yaml_path, d, 0644)
+}
---
+++ pkg/model/policy/policy_test.go
@@ -0,0 +1,52 @@
+package policy
+
+import (
+	"testing"
+)
+
+func TestGetPolicy(t *testing.T) {
+	t.Log("Testing get policy for broadwell cpu ... ")
+
+	SetPolicyFilePath("../../../etc/policy.yaml")
+
+	policy, err := GetPolicy("broadwell")
+
+	if err != nil {
+		t.Errorf("Failed to get policy for broadwell")
+	}
+
+	if policy.Gold.Size != 15 {
+		t.Errorf("wrong policy for broadwell")
+	}
+}
+
+func TestUpdatePolicy(t *testing.T) {
+	t.Log("Testing update policy for broadwell cpu ... ")
+	SetPolicyFilePath("../../../etc/policy.yaml")
+
+	policy, err := GetPolicy("broadwell")
+
+	if err != nil {
+		t.Errorf("Failed to get policy for broadwell")
+	}
+
+	policy.Gold.Size = 1
+
+	err = UpdatePolicy("broadwell", &policy)
+
+	if err != nil {
+		t.Errorf("Failed to Update policy for broadwell")
+	}
+	new_policy, err := GetPolicy("broadwell")
+
+	if err != nil {
+		t.Errorf("Failed to get policy for broadwell")
+	}
+	if new_policy.Gold.Size != 1 {
+		t.Errorf("Failed to update policy")
+	}
+
+	// Write the origin value back
+	new_policy.Gold.Size = 15
+	UpdatePolicy("broadwell", &new_policy)
+}
