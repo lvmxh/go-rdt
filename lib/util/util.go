a992e0b31d145ae5b8704e50b9db4d0ffed8a923
Feng, Shaohe <shaohe.feng@intel.com>
2017-04-27 17:27:53 +0800

    add type convert function.
    will refactor soem setField code to this new function.

--- lib/util/util.go
+++ lib/util/util.go
@@ -6,6 +6,32 @@ import (
 	"strconv"
 )
 
+func typeConversion(value string, ntype string) (reflect.Value, error) {
+	if ntype == "string" {
+		return reflect.ValueOf(value), nil
+	} else if ntype == "int" {
+		i, err := strconv.Atoi(value)
+		return reflect.ValueOf(i), err
+	} else if ntype == "int8" {
+		i, err := strconv.ParseInt(value, 10, 64)
+		return reflect.ValueOf(int8(i)), err
+	} else if ntype == "int32" {
+		i, err := strconv.ParseInt(value, 10, 64)
+		return reflect.ValueOf(int64(i)), err
+	} else if ntype == "int64" {
+		i, err := strconv.ParseInt(value, 10, 64)
+		return reflect.ValueOf(i), err
+	} else if ntype == "float32" {
+		i, err := strconv.ParseFloat(value, 64)
+		return reflect.ValueOf(float32(i)), err
+	} else if ntype == "float64" {
+		i, err := strconv.ParseFloat(value, 64)
+		return reflect.ValueOf(i), err
+	}
+	//else if ....... add other type
+	return reflect.ValueOf(value), fmt.Errorf("unknow type" + ntype)
+}
+
 // Set obj's 'name' field with proper type
 func SetField(obj interface{}, name string, value interface{}) error {
 	structValue := reflect.ValueOf(obj).Elem()
@@ -23,6 +49,7 @@ func SetField(obj interface{}, name string, value interface{}) error {
 	structFieldType := structFieldValue.Type()
 
 	switch structFieldType.Name() {
+	// Try to remove these logic to TypeConversion
 	case "int":
 		v := value.(string)
 		v_int, err := strconv.Atoi(v)
