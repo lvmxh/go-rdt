4dad356b6bfa3e79e6a915bde7eb39deee045d47
Qiao, Liyong <liyong.qiao@intel.com>
2017-06-28 14:20:39 +0800

    Config: Cleanup config usage
    Cleanup mess configuration usage.
    
    The configurations comes from command line options and also
    configuration file, merge them into a RDAgent config file.
    
    Thanks Dakshina for the ideas, adds as co-author.
    
    Co-Authored-By: Dakshina Ilangovan <dakshina.ilangovan@intel.com>
    Signed-off-by: Qiao, Liyong <liyong.qiao@intel.com>

--- app/server.go
+++ app/server.go
@@ -12,13 +12,10 @@ import (
 	"github.com/emicklei/go-restful-swagger12"
 	"openstackcore-rdtagent/api/v1"
 	"openstackcore-rdtagent/db"
-	"openstackcore-rdtagent/model/capabilities"
 	"openstackcore-rdtagent/util/options"
 )
 
-// TODO move this out of app server
-type GenericAPIConfig struct {
-	//APIServerServiceIP   net.IP
+type GenericConfig struct {
 	APIServerServiceIP   string
 	APIServerServicePort string
 	EnableUISupport      bool
@@ -27,96 +24,60 @@ type GenericAPIConfig struct {
 	DBName               string
 }
 
-// TODO move this out of app server
-// This is server config, include any running status data in this struct
-// eg: as far as I can see, cpuinfo, rdt caps
+// RDAgent config
 type Config struct {
-	GenericConfig *GenericAPIConfig
-	SwaggerConfig *swagger.Config
+	Generic *GenericConfig
+	Swagger *swagger.Config
 }
 
-// TODO move this out of app server
-type completedConfig struct {
-	*Config
-}
-
-// TODO move this out of app server
-type APIServer struct {
-	HandlerContainer *restful.Container
-	Server           *http.Server
-}
-
-// TODO move this out of app server
-func NewAPIConfig() *GenericAPIConfig {
-	return &GenericAPIConfig{
-		APIServerServiceIP:   "localhost",
-		APIServerServicePort: "8080",
-		EnableUISupport:      true,
-	}
-}
-
-func BuildServerConfig(s *options.ServerRunOptions) (*Config, error) {
-	apiconfig := NewAPIConfig()
-
+// build RDAgent configuration from command line and configure file
+func BuildServerConfig(s *options.ServerRunOptions) *Config {
 	// FIXME (cmd line options does not override the config file options)
 	appconfig := appConf.NewConfig()
+
 	if s.Addr == "" {
 		s.Addr = appconfig.Def.Address
 	}
-	apiconfig.APIServerServiceIP = s.Addr
+
 	if s.Port == "" {
 		s.Port = strconv.FormatUint(uint64(appconfig.Def.Port), 10)
 	}
-	apiconfig.APIServerServicePort = s.Port
-	apiconfig.DBBackend = appconfig.Db.Backend
-	apiconfig.Transport = appconfig.Db.Transport
-	apiconfig.DBName = appconfig.Db.DBName
 
-	weburl := fmt.Sprintf("http://%s:%s", s.Addr, s.Port)
+	genericconfig := GenericConfig{
+		APIServerServiceIP:   s.Addr,
+		APIServerServicePort: s.Port,
+		DBBackend:            appconfig.Db.Backend,
+		Transport:            appconfig.Db.Transport,
+		DBName:               appconfig.Db.DBName,
+	}
+
 	swaggerconfig := swagger.Config{
-		WebServicesUrl: weburl,
+		WebServicesUrl: fmt.Sprintf("http://%s:%s", s.Addr, s.Port),
 		ApiPath:        "/apidocs.json",
-		// Optionally, specifiy where the UI is located
-		SwaggerPath: "/apidocs/",
+		SwaggerPath:    "/apidocs/", // Optionally, specifiy where the UI is located
 		// FIXME (eliqiao): this depends on https://github.com/swagger-api/swagger-ui.git need to copy dist from it
 		SwaggerFilePath: "/usr/local/share/go/src/github.com/wordnik/swagger-ui/dist",
 		ApiVersion:      "1.0",
 	}
 
-	config := &Config{
-		GenericConfig: apiconfig,
-		SwaggerConfig: &swaggerconfig,
+	return &Config{
+		Generic: &genericconfig,
+		Swagger: &swaggerconfig,
 	}
-	return config, nil
 }
 
-func (c *Config) Complete() completedConfig {
-	// TODO to complete config in this function
-
-	// test init capabilities
-	l3cat := capabilities.L3CAT{
-		NumCLOS: 16,
-		NumWays: 20,
-		WaySize: 10000,
-	}
-
-	capabilities.Setup(nil, &l3cat, nil)
-
-	return completedConfig{c}
-}
-
-func InitializeDB(c completedConfig) (db.DB, error) {
+func InitializeDB(c *Config) (db.DB, error) {
 	var d db.DB
 
-	if c.Config.GenericConfig.DBBackend == "bolt" {
+	if c.Generic.DBBackend == "bolt" {
 		d = new(db.BoltDB)
-	} else if c.Config.GenericConfig.DBBackend == "mgo" {
+	} else if c.Generic.DBBackend == "mgo" {
 		d = new(db.MgoDB)
 	} else {
-		return nil, fmt.Errorf("Unsupported DB backend %s", c.Config.GenericConfig.DBBackend)
+		return nil, fmt.Errorf("Unsupported DB backend %s", c.Generic.DBBackend)
 	}
 
-	err := d.Initialize(c.Config.GenericConfig.Transport, c.Config.GenericConfig.DBName)
+	err := d.Initialize(c.Generic.Transport, c.Generic.DBName)
 
 	if err != nil {
 		return nil, err
@@ -124,8 +85,10 @@ func InitializeDB(c completedConfig) (db.DB, error) {
 	return d, nil
 }
 
-func (c completedConfig) New() (*APIServer, error) {
+// Initialize server from config
+func Initialize(c *Config) (*http.Server, error) {
 	db, err := InitializeDB(c)
+
 	if err != nil {
 		return nil, err
 	}
@@ -145,22 +108,21 @@ func (c completedConfig) New() (*APIServer, error) {
 	wls.Register(wsContainer)
 
 	// Install adds the SgaggerUI webservices
-	c.Config.SwaggerConfig.WebServices = wsContainer.RegisteredWebServices()
-	swagger.RegisterSwaggerService(*(c.Config.SwaggerConfig), wsContainer)
+	c.Swagger.WebServices = wsContainer.RegisteredWebServices()
+	swagger.RegisterSwaggerService(*(c.Swagger), wsContainer)
 
 	// TODO error handle
-	return &APIServer{
-		HandlerContainer: wsContainer,
-		Server:           &http.Server{Addr: c.Config.GenericConfig.APIServerServiceIP + ":" + c.Config.GenericConfig.APIServerServicePort, Handler: wsContainer},
-	}, nil
+	return &http.Server{Addr: c.Generic.APIServerServiceIP + ":" + c.Generic.APIServerServicePort, Handler: wsContainer}, nil
 }
 
 // RunServer uses the provided options to run the apiserver.
 func RunServer(s *options.ServerRunOptions) {
-	// TODO handle err
-	config, _ := BuildServerConfig(s)
 
-	ser, _ := config.Complete().New()
-	fmt.Println("started")
-	log.Fatal(ser.Server.ListenAndServe())
+	config := BuildServerConfig(s)
+	server, err := Initialize(config)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Fatal(server.ListenAndServe())
+	}
 }
