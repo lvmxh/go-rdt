604e62f64b033c1b8c73233cd9b37b9b57f5177b
Qiao, Liyong <liyong.qiao@intel.com>
2017-08-23 13:38:08 +0800

    Introduce ginkgo test suite for flock
    ginkgo is another BDD test framwork, which is similar with goconvey,
    this patch is just an example, added flock test case (use example), it
    implact nothing.
    
    `go test -v`
    or
    `ginkgo -v` to run test suite
    
    Signed-off-by: Qiao, Liyong <liyong.qiao@intel.com>

--- lib/flock/flock_test.go
+++ lib/flock/flock_test.go
@@ -1,26 +1,46 @@
-package flock
+package flock_test
 
 import (
-	"fmt"
 	"os"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "openstackcore-rdtagent/lib/flock"
 )
 
-// we need to add Test case instead of Example.
-func ExampleFlock() {
-	fname := "./test.txt"
-	flag := os.O_RDWR | os.O_CREATE
-	f, e := os.OpenFile(fname, flag, 0600)
-	if e != nil {
-		fmt.Printf("Failed to create a file. %v", e)
-	}
-	defer f.Close()
+var _ = Describe("Flock", func() {
 
-	// FIXME(Shaohe, Feng): does not find to run a process to flock the file.
-	// such as: exec.Command("flock", "-x", fname, "-c", "cat")
-	// flock -x test.txt -c cat
-	fmt.Println(Flock(f, 1000*1000*1000, true))
-	fmt.Println(Funlock(f))
-	// Output:
-	// <nil>
-	// <nil>
-}
+	var (
+		fname string
+		f     *os.File
+		err   error
+	)
+
+	BeforeEach(func() {
+		fname = "./test.txt"
+	})
+
+	AfterEach(func() {
+		Funlock(f)
+		f.Close()
+		os.Remove(fname)
+	})
+
+	// Separating Creation and Configuration
+	JustBeforeEach(func() {
+		flag := os.O_RDWR | os.O_CREATE
+		f, err = os.OpenFile(fname, flag, 0600)
+		if err != nil {
+			Fail("Failed to open file")
+		}
+		err = Flock(f, 1000*1000*1000, true)
+	})
+
+	Describe("Showing how to use Flock", func() {
+		It("should not error", func() {
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+})
