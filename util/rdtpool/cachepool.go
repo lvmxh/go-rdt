8ca7a155fca509c44340de6b90bb7ec6d1e2c163
Qiao, Liyong <liyong.qiao@intel.com>
2017-08-28 10:34:05 +0800

    rdtpool: Restruct cache pool
    Change cache pool to separate Guarantee, Besteffort, Shared to not have
    overlap with each other.
    
    Restrict that cache ways of Guarantee+Besteffort+Shared+osReserved <=
    total ways of the host.
    
    Reduce duplication code.
    
    Signed-off-by: Qiao, Liyong <liyong.qiao@intel.com>

--- util/rdtpool/cachepool.go
+++ util/rdtpool/cachepool.go
@@ -14,6 +14,39 @@ import (
 var cachePoolReserved = make(map[string]*Reserved, 0)
 var cachePoolOnce sync.Once
 
+// helper function to get Reserved resource
+func getReservedCache(
+	wayCandidate int,
+	wayOffset, osCacheWays uint,
+	osCPUbm *util.Bitmap,
+	sysc map[string]syscache.SysCache) (*Reserved, error) {
+
+	r := &Reserved{}
+
+	schemata := map[string]*util.Bitmap{}
+	osCPUs := map[string]*util.Bitmap{}
+	wc := wayCandidate
+	var err error
+
+	for _, sc := range sysc {
+		bm, _ := CpuBitmaps([]string{sc.SharedCpuList})
+		osCPUs[sc.Id] = osCPUbm.And(bm)
+		// no os group on this cache id
+		if !osCPUs[sc.Id].IsEmpty() {
+			wc = wc << osCacheWays
+		}
+		wc = wc << wayOffset
+		mask := strconv.FormatUint(uint64(wc), 16)
+		schemata[sc.Id], err = CacheBitmaps(mask)
+		if err != nil {
+			return r, err
+		}
+	}
+
+	r.Schemata = schemata
+	return r, nil
+}
+
 func GetCachePoolLayout() (map[string]*Reserved, error) {
 	var return_err error
 	cachePoolOnce.Do(func() {
@@ -21,16 +54,9 @@ func GetCachePoolLayout() (map[string]*Reserved, error) {
 		osConf := NewOSConfig()
 		ways := GetCosInfo().CbmMaskLen
 
-		if osConf.CacheWays+poolConf.Guarantee+poolConf.Besteffort > uint(ways) {
+		if osConf.CacheWays+poolConf.Guarantee+poolConf.Besteffort+poolConf.Shared > uint(ways) {
 			return_err = fmt.Errorf(
-				"Error config: Guarantee + Besteffort + OS reserved ways should be less or equal to %d.", ways)
-			return
-		}
-
-		if poolConf.Besteffort < poolConf.Shared {
-			return_err = fmt.Errorf(
-				"Error config: Shared ways %d should be less or equal to Besteffort ways %d.",
-				poolConf.Shared, poolConf.Besteffort)
+				"Error config: Guarantee + Besteffort + Shared + OS reserved ways should be less or equal to %d.", ways)
 			return
 		}
 
@@ -44,87 +70,50 @@ func GetCachePoolLayout() (map[string]*Reserved, error) {
 			return
 		}
 
-		// FIXME: improve this logic
 		if poolConf.Guarantee > 0 {
-			schemata := map[string]*util.Bitmap{}
-			osCPUs := map[string]*util.Bitmap{}
-
-			for _, sc := range syscaches {
-				bm, _ := CpuBitmaps([]string{sc.SharedCpuList})
-				osCPUs[sc.Id] = osCPUbm.And(bm)
-
-				wayCandidate := 1<<poolConf.Guarantee - 1
-				// no os group on this cache id
-				if !osCPUs[sc.Id].IsEmpty() {
-					wayCandidate = wayCandidate << osConf.CacheWays
-				}
-				mask := strconv.FormatUint(uint64(wayCandidate), 16)
-				schemata[sc.Id], return_err = CacheBitmaps(mask)
-				if return_err != nil {
-					return
-				}
+			wc := 1<<poolConf.Guarantee - 1
+			resev, err := getReservedCache(wc,
+				0,
+				osConf.CacheWays,
+				osCPUbm,
+				syscaches)
+			if err != nil {
+				return_err = err
+				return
 			}
-			guaranteePoolReserved := &Reserved{}
-			guaranteePoolReserved.Schemata = schemata
-
-			cachePoolReserved[Guarantee] = guaranteePoolReserved
+			cachePoolReserved[Guarantee] = resev
 		}
 
-		// FIXME: improve this logic
 		if poolConf.Besteffort > 0 {
-			schemata := map[string]*util.Bitmap{}
-			osCPUs := map[string]*util.Bitmap{}
+			wc := 1<<poolConf.Besteffort - 1
+			resev, err := getReservedCache(wc,
+				poolConf.Guarantee,
+				osConf.CacheWays,
+				osCPUbm,
+				syscaches)
 
-			for _, sc := range syscaches {
-				bm, _ := CpuBitmaps([]string{sc.SharedCpuList})
-				osCPUs[sc.Id] = osCPUbm.And(bm)
-
-				wayCandidate := 1<<poolConf.Besteffort - 1
-				wayCandidate = wayCandidate << poolConf.Guarantee
-				// no os group on this cache id
-				if !osCPUs[sc.Id].IsEmpty() {
-					wayCandidate = wayCandidate << osConf.CacheWays
-				}
-				mask := strconv.FormatUint(uint64(wayCandidate), 16)
-				schemata[sc.Id], return_err = CacheBitmaps(mask)
-				if return_err != nil {
-					return
-				}
+			if err != nil {
+				return_err = err
+				return
 			}
-			besteffortPoolReserved := &Reserved{}
-			besteffortPoolReserved.Schemata = schemata
-
-			cachePoolReserved[Besteffort] = besteffortPoolReserved
+			cachePoolReserved[Besteffort] = resev
 		}
 
-		// FIXME: improve this logic
 		if poolConf.Shared > 0 {
-			schemata := map[string]*util.Bitmap{}
-			osCPUs := map[string]*util.Bitmap{}
+			wc := 1<<poolConf.Shared - 1
+			resev, err := getReservedCache(wc,
+				poolConf.Guarantee+poolConf.Besteffort,
+				osConf.CacheWays,
+				osCPUbm,
+				syscaches)
 
-			for _, sc := range syscaches {
-				bm, _ := CpuBitmaps([]string{sc.SharedCpuList})
-				osCPUs[sc.Id] = osCPUbm.And(bm)
-				wayCandidate := 1<<poolConf.Shared - 1
-				offset := (poolConf.Besteffort - poolConf.Shared) / 2
-				wayToMove := poolConf.Guarantee + poolConf.Besteffort - poolConf.Shared - offset
-				wayCandidate = wayCandidate << wayToMove
-				if !osCPUs[sc.Id].IsEmpty() {
-					wayCandidate = wayCandidate << osConf.CacheWays
-				}
-
-				mask := strconv.FormatUint(uint64(wayCandidate), 16)
-				schemata[sc.Id], return_err = CacheBitmaps(mask)
-				if return_err != nil {
-					return
-				}
+			if err != nil {
+				return_err = err
+				return
 			}
-
-			sharedPoolReserved := &Reserved{Quota: poolConf.MaxAllowedShared}
-			sharedPoolReserved.Schemata = schemata
-			sharedPoolReserved.Name = Shared
-
-			cachePoolReserved[Shared] = sharedPoolReserved
+			cachePoolReserved[Shared] = resev
+			cachePoolReserved[Shared].Name = Shared
+			cachePoolReserved[Shared].Quota = poolConf.MaxAllowedShared
 		}
 	})
 
