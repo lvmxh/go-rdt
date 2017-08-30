9f455559a9946efd84999b28badf0a3d3a882235
Qiao, Liyong <liyong.qiao@intel.com>
2017-08-30 18:24:13 +0800

    Fix return value of hacking.sh
    golint returns error message if error happends but always 0.
    
    Signed-off-by: Qiao, Liyong <liyong.qiao@intel.com>

--- hacking.sh
+++ hacking.sh
@@ -8,14 +8,18 @@ files=$(git diff HEAD~1 --stat | awk '{if ($1 ~ /\.go$/) {print $1}}')
 
 arr=($files)
 
+ret=0
 echo "calling golint on all changed files"
 for f in "${arr[@]}"
 do
     echo "calling golint $f ..."
-    golint "$f"
+    rev=$(golint "$f")
+    if [[ ! -z $rev ]]; then
+        ret=-1
+    fi
 done
 
-if [[ $? -ne 0 ]]; then
+if [[ $ret -ne 0 ]]; then
     echo ":( <<< Please address coding style mistakes."
 else
     echo ":) >>> No errors for coding style"
