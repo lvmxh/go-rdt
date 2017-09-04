7a690c801aab3062dec36211e8831ce9dd825716
Shaohe Feng <shaohe.feng@intel.com>
2017-09-03 23:54:22 -0400

    add cache/llc api
    Does not find how to use internal redirect for llc.

--- api/v1/caches.go
+++ api/v1/caches.go
@@ -2,12 +2,14 @@ package v1
 
 import (
 	"fmt"
-	log "github.com/sirupsen/logrus"
 	"net/http"
 	"strconv"
 	"strings"
 
 	"github.com/emicklei/go-restful"
+	log "github.com/sirupsen/logrus"
+
+	"openstackcore-rdtagent/lib/cache"
 	m_cache "openstackcore-rdtagent/model/cache"
 )
 
@@ -17,6 +19,19 @@ type CachesResource struct{}
 // Cache Level Info
 // This should merge into CachesResource
 type CachesLevelResource struct {
+}
+
+func getCacheLevelFromUrl(request *restful.Request) uint32 {
+	var ilev uint32
+	slev := strings.TrimLeft(request.PathParameter("cache-level"), "l")
+	if slev == "c" {
+		ilev = syscache.GetLLC()
+	} else {
+		// go-resfull re is a gate for badrequest. so no err here.
+		l, _ := strconv.Atoi(slev)
+		ilev = uint32(l)
+	}
+	return ilev
 }
 
 func (cache CachesResource) Register(container *restful.Container) {
@@ -32,14 +47,14 @@ func (cache CachesResource) Register(container *restful.Container) {
 		Operation("CachesGet").
 		Writes(CachesResource{}))
 
-	ws.Route(ws.GET("/{cache-level:^l[2-3]$}").To(cache.CachesLevelGet).
+	ws.Route(ws.GET("/{cache-level:^l([2-3]|lc)$}").To(cache.CachesLevelGet).
 		Doc("Get the info of a specified level cache.").
 		Param(ws.PathParameter("cache-level", "cache level").DataType("string")).
 		Operation("CachesLevelGet").
 		Writes(CachesLevelResource{}))
 
 	// FIXME (Shaohe): should use pattern, \d\{1,3\}
-	ws.Route(ws.GET("/{cache-level:^l[2-3]$}/{id:^[0-9]{1,9}$").To(cache.CacheGet).
+	ws.Route(ws.GET("/{cache-level:^l([2-3]|lc)$}/{id:^[0-9]{1,9}$").To(cache.CacheGet).
 		Doc("Get the info of a specified cache.").
 		Param(ws.PathParameter("cache-level", "cache level").DataType("string")).
 		Param(ws.PathParameter("id", "cache id").DataType("uint")).
@@ -65,21 +80,16 @@ func (cache CachesResource) CachesGet(request *restful.Request, response *restfu
 // GET /v1/cache/l[2, 3]
 func (cache CachesResource) CachesLevelGet(request *restful.Request, response *restful.Response) {
 	c := &m_cache.CacheInfos{}
-	ilev, err := strconv.Atoi(strings.TrimLeft(request.PathParameter("cache-level"), "l"))
 
-	if err != nil {
-		response.WriteError(http.StatusBadRequest, err)
-		return
-	}
-
+	ilev := getCacheLevelFromUrl(request)
 	log.Printf("Request Level %d", ilev)
-
-	err = c.GetByLevel(uint32(ilev))
+	err := c.GetByLevel(ilev)
 	// FIXME (Shaohe): We should classify the error.
 	if err != nil {
 		response.WriteError(http.StatusInternalServerError, err)
 		return
 	}
+
 	response.WriteEntity(c)
 }
 
@@ -87,8 +97,7 @@ func (cache CachesResource) CachesLevelGet(request *restful.Request, response *r
 func (cache CachesResource) CacheGet(request *restful.Request, response *restful.Response) {
 	c := &m_cache.CacheInfos{}
 
-	level := request.PathParameter("cache-level")
-
+	ilev := getCacheLevelFromUrl(request)
 	// FIXME (Shaohe): should use pattern, \d\{1,3\}
 	id, err := strconv.Atoi(request.PathParameter("id"))
 	if err != nil {
@@ -96,11 +105,9 @@ func (cache CachesResource) CacheGet(request *restful.Request, response *restful
 		response.WriteError(http.StatusBadRequest, err)
 		return
 	}
-	log.Printf("Request Level%s, id: %d\n", strings.TrimLeft(level, "l"), id)
+	log.Printf("Request Level%d, id: %d\n", ilev, id)
 
-	ilev, _ := strconv.Atoi(strings.TrimLeft(level, "l"))
-
-	err = c.GetByLevel(uint32(ilev))
+	err = c.GetByLevel(ilev)
 	// FIXME (Shaohe): We should classify the error.
 	if err != nil {
 		response.WriteError(http.StatusInternalServerError, err)
