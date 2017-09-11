916be6bfa614726f9fbbd6b2e8936b7cdf86bea7
Qiao, Liyong <liyong.qiao@intel.com>
2017-09-11 16:34:31 +0800

    Add more test case for cache api testing
    should be 400 for bad llc endpoint.
    
    Signed-off-by: Qiao, Liyong <liyong.qiao@intel.com>

--- test/integration/caches_test.go
+++ test/integration/caches_test.go
@@ -2,10 +2,13 @@ package integration_test
 
 import (
 	"net/http"
+	"strconv"
 
 	. "github.com/onsi/ginkgo"
 
 	"gopkg.in/gavv/httpexpect.v1"
+
+	"openstackcore-rdtagent/lib/cache"
 	"openstackcore-rdtagent/test/integration/test_helpers"
 )
 
@@ -41,23 +44,31 @@ var cacheSchemaTemplate string = `{
 	"required": ["rdt", "cqm", "cdp", "cdp_enable", "cat", "cat_enable", "caches"]
 }`
 
+// Caches is capital ? fix it ?
+var cacheLevelSchemaTemplate string = `{
+	"type": "object",
+	"properties": {
+		"number": {{.pint}},
+		"Caches": {
+			"type": "object"
+		}
+	}
+}`
+
 var _ = Describe("Caches", func() {
 
 	var (
-		v1url       string
-		he          *httpexpect.Expect
-		cacheSchema string
+		v1url            string
+		he               *httpexpect.Expect
+		cacheSchema      string
+		cacheLevelSchema string
+		llc              uint32
 	)
 
 	BeforeEach(func() {
 		By("set url")
 		v1url = testhelpers.GetV1URL()
 		he = httpexpect.New(GinkgoT(), v1url)
-		cacheSchema, _ = testhelpers.FormatByKey(cacheSchemaTemplate,
-			map[string]interface{}{
-				"bool": testhelpers.BoolSchema,
-				"pint": testhelpers.PositiveInteger,
-				"uint": testhelpers.NonNegativeInteger})
 	})
 
 	AfterEach(func() {
@@ -65,6 +76,15 @@ var _ = Describe("Caches", func() {
 
 	Describe("Get the new system", func() {
 		Context("when request 'cache' API", func() {
+			BeforeEach(func() {
+				By("Set cache schemata")
+				cacheSchema, _ = testhelpers.FormatByKey(cacheSchemaTemplate,
+					map[string]interface{}{
+						"bool": testhelpers.BoolSchema,
+						"pint": testhelpers.PositiveInteger,
+						"uint": testhelpers.NonNegativeInteger})
+			})
+
 			It("Should be return 200", func() {
 
 				repos := he.GET("/cache").
@@ -75,6 +95,54 @@ var _ = Describe("Caches", func() {
 
 				repos.Schema(cacheSchema)
 			})
+		})
+
+		Context("when request 'cache' API by level", func() {
+			BeforeEach(func() {
+				By("Set cache level schemata")
+				cacheLevelSchema, _ = testhelpers.FormatByKey(cacheLevelSchemaTemplate,
+					map[string]interface{}{
+						"pint": testhelpers.PositiveInteger,
+					})
+
+				llc = syscache.GetLLC()
+			})
+
+			It("Should be return 200 for llc ", func() {
+
+				repos := he.GET("/cache/llc").
+					WithHeader("Content-Type", "application/json").
+					Expect().
+					Status(http.StatusOK).
+					JSON()
+				repos.Schema(cacheLevelSchema)
+			})
+
+			It("Should be return 200 for last level", func() {
+
+				target_lev := strconv.FormatUint(uint64(llc), 10)
+				cacheUrl := "/cache/l" + target_lev
+
+				repos := he.GET(cacheUrl).
+					WithHeader("Content-Type", "application/json").
+					Expect().
+					Status(http.StatusOK).
+					JSON()
+				repos.Schema(cacheLevelSchema)
+			})
+
+			It("Should be return 400 for not last level", func() {
+
+				badllc := llc - 1
+				target_lev := strconv.FormatUint(uint64(badllc), 10)
+				cacheUrl := "/cache/l" + target_lev
+
+				he.GET(cacheUrl).
+					WithHeader("Content-Type", "application/json").
+					Expect().
+					Status(http.StatusBadRequest)
+			})
+
 		})
 	})
 })
