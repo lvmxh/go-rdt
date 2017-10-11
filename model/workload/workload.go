445313bd6a3ca6f0c8f25dd5c2769f52c44a46ed
Qiao, Liyong <liyong.qiao@intel.com>
2017-10-11 16:44:41 +0800

    Fix: use rpc client when accessing resource group
    Got this error when create workload:
    
    Error to commit resource group for workload.. Details: mkdir
    /sys/fs/resctrl/74862-guarantee: permission denied
    
    API need to leverage proxyclient to call root process operate resource
    group.
    
    Signed-off-by: Qiao, Liyong <liyong.qiao@intel.com>

--- model/workload/workload.go
+++ model/workload/workload.go
@@ -14,6 +14,7 @@ import (
 	libcache "openstackcore-rdtagent/lib/cache"
 	"openstackcore-rdtagent/lib/cpu"
 	"openstackcore-rdtagent/lib/proc"
+	"openstackcore-rdtagent/lib/proxyclient"
 	"openstackcore-rdtagent/lib/resctrl"
 	libutil "openstackcore-rdtagent/lib/util"
 
@@ -60,7 +61,7 @@ func Enforce(w *tw.RDTWorkLoad) *AppError {
 
 	l.Lock()
 	defer l.Unlock()
-	resaall := resctrl.GetResAssociation()
+	resaall := proxyclient.GetResAssociation()
 
 	er := &tw.EnforceRequest{}
 	if err := populateEnforceRequest(er, w); err != nil {
@@ -172,7 +173,7 @@ func Enforce(w *tw.RDTWorkLoad) *AppError {
 	}
 	resAss.Tasks = append(resAss.Tasks, w.TaskIDs...)
 
-	if err = resctrl.Commit(resAss, grpName); err != nil {
+	if err = proxyclient.Commit(resAss, grpName); err != nil {
 		log.Errorf("Error while try to commit resource group for workload %s, group name %s", w.ID, grpName)
 		return NewAppError(http.StatusInternalServerError,
 			"Error to commit resource group for workload.", err)
@@ -185,9 +186,9 @@ func Enforce(w *tw.RDTWorkLoad) *AppError {
 	// possible fix is to adding this into a task flow
 	for name, res := range changedRes {
 		log.Debugf("Shink %s group", name)
-		if err = resctrl.Commit(res, name); err != nil {
+		if err = proxyclient.Commit(res, name); err != nil {
 			log.Errorf("Error while try to commit shrinked resource group, name: %s", name)
-			resctrl.DestroyResAssociation(grpName)
+			proxyclient.DestroyResAssociation(grpName)
 			return NewAppError(http.StatusInternalServerError,
 				"Error to shrink resource group", err)
 		}
@@ -196,7 +197,7 @@ func Enforce(w *tw.RDTWorkLoad) *AppError {
 	// reset os group
 	if err = rdtpool.SetOSGroup(); err != nil {
 		log.Errorf("Error while try to commit resource group for default group")
-		resctrl.DestroyResAssociation(grpName)
+		proxyclient.DestroyResAssociation(grpName)
 		return NewAppError(http.StatusInternalServerError,
 			"Error while try to commit resource group for default group.", err)
 	}
@@ -211,7 +212,7 @@ func Release(w *tw.RDTWorkLoad) error {
 	l.Lock()
 	defer l.Unlock()
 
-	resaall := resctrl.GetResAssociation()
+	resaall := proxyclient.GetResAssociation()
 
 	r, ok := resaall[w.CosName]
 
@@ -231,7 +232,7 @@ func Release(w *tw.RDTWorkLoad) error {
 	// safely remove resource group if no tasks and cpu bit map is empty
 	if len(r.Tasks) < 1 && cpubm.IsEmpty() {
 		log.Printf("Remove resource group: %s", w.CosName)
-		if err := resctrl.DestroyResAssociation(w.CosName); err != nil {
+		if err := proxyclient.DestroyResAssociation(w.CosName); err != nil {
 			return err
 		}
 		if err := rdtpool.SetOSGroup(); err != nil {
@@ -241,14 +242,14 @@ func Release(w *tw.RDTWorkLoad) error {
 	}
 	// remove workload task ids from resource group
 	if len(w.TaskIDs) > 0 {
-		if err := resctrl.RemoveTasks(w.TaskIDs); err != nil {
+		if err := proxyclient.RemoveTasks(w.TaskIDs); err != nil {
 			log.Printf("Ignore Error while remove tasks %s", err)
 			return nil
 		}
 	}
 	if len(w.CoreIDs) > 0 {
 		r.CPUs = cpubm.ToString()
-		return resctrl.Commit(r, w.CosName)
+		return proxyclient.Commit(r, w.CosName)
 	}
 	return nil
 }
@@ -303,7 +304,7 @@ func Update(w, patched *tw.RDTWorkLoad) *AppError {
 
 	l.Lock()
 	defer l.Unlock()
-	resaall := resctrl.GetResAssociation()
+	resaall := proxyclient.GetResAssociation()
 
 	if !reflect.DeepEqual(patched.CoreIDs, w.CoreIDs) ||
 		!reflect.DeepEqual(patched.TaskIDs, w.TaskIDs) {
@@ -334,7 +335,7 @@ func Update(w, patched *tw.RDTWorkLoad) *AppError {
 			w.CoreIDs = patched.CoreIDs
 		}
 		// commit changes
-		if err = resctrl.Commit(targetResAss, w.CosName); err != nil {
+		if err = proxyclient.Commit(targetResAss, w.CosName); err != nil {
 			log.Errorf("Error while try to commit resource group for workload %s, group name %s", w.ID, w.CosName)
 			return NewAppError(http.StatusInternalServerError,
 				"Error to commit resource group for workload.", err)
