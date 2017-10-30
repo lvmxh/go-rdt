b3fad4ca0eccf0cd0da0a85e14d3f9532af0b234
Qiao, Liyong <liyong.qiao@intel.com>
2017-10-30 10:14:14 +0800

    Fix workload test case
    Fix test case for getCacheIDs

--- model/workload/workload_test.go
+++ model/workload/workload_test.go
@@ -20,25 +20,25 @@ func TestGetCacheIDs(t *testing.T) {
 
 	cpubitmap := "3"
 
-	cache_ids := getCacheIDs(cpubitmap, cacheinfos, 8)
+	cache_ids := getCacheIDs([]string{}, cpubitmap, cacheinfos, 8)
 	if len(cache_ids) != 1 && cache_ids[0] != 0 {
 		t.Errorf("cache_ids should be [0], but we get %v", cache_ids)
 	}
 
 	cpubitmap = "1f"
-	cache_ids = getCacheIDs(cpubitmap, cacheinfos, 8)
+	cache_ids = getCacheIDs([]string{}, cpubitmap, cacheinfos, 8)
 	if len(cache_ids) != 2 {
 		t.Errorf("cache_ids should be [0, 1], but we get %v", cache_ids)
 	}
 
 	cpubitmap = "10"
-	cache_ids = getCacheIDs(cpubitmap, cacheinfos, 8)
+	cache_ids = getCacheIDs([]string{}, cpubitmap, cacheinfos, 8)
 	if len(cache_ids) != 1 && cache_ids[0] != 1 {
 		t.Errorf("cache_ids should be [1], but we get %v", cache_ids)
 	}
 
 	cpubitmap = "f00"
-	cache_ids = getCacheIDs(cpubitmap, cacheinfos, 8)
+	cache_ids = getCacheIDs([]string{}, cpubitmap, cacheinfos, 8)
 	if len(cache_ids) != 0 {
 		t.Errorf("cache_ids should be [], but we get %v", cache_ids)
 	}
